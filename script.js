0ea77d705309def8f6ded776088701b12c1b87cc
Dinara <dinaralinnyk@gmail.com>
2026-09-24 11:22:40 +0000

    Add cloud service and JavaScript functionality

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function showMessage() {
     message.textContent =
         "JavaScript працює. Хмарний застосунок успішно запущено!";
 }
+
+function showCloudInfo() {
+    document.getElementById("cloudInfo").textContent =
+        "Хмарні обчислення — це модель надання обчислювальних ресурсів через мережу.";
+}
